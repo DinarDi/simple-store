3d1c5ea4bc699f61a6a1fca0f7673c430ca2b6d7
DinarDi <bugagafree@gmail.com>
2021-09-22 17:19:13 +0500

    modified

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { IAllItems, IOrders } from "../types";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://store-simple.herokuapp.com/",
 });
 
 export const fetchAPI = {
